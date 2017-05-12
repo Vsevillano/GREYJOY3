672e9716d1e532db225b918cc5f58d38a1acd972
Rafa Delgado <rafadelgado89@gmail.com>
2017-05-12 08:58:22 +0200

    Primera modificación
    Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,12 +1,14 @@
-﻿/**
+﻿
+
+package holaMundo;
+//Hay que importar el Scanner, se acuerda alguien??
+/**
 * Esto es una calculadora
 * Colaboradores: Rafa Delgado, Javier Ponferrada, Victor Sevillano, Guillermo Boquizo
 *
 * @author Rafael Delgado Peña
 * @version 1.0
 */
-
-package holaMundo;
 
 class HolaMundo {
 	public static void main (String[] args) {
