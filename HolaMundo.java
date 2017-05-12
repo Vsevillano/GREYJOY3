bfcf33e4f0345c3bbb7ebfd95a07412dbc9fbfbc
rdp89 <rafadelgado89@gmail.com>
2017-05-12 18:50:44 +0200

    Más autores
    Añadida línea con todos los colaboradores

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,7 @@
 * Muestra por pantalla la frase "¡Hola mundo!"
 *
 * @author Victoriano Sevillano Vega
+* Más autores: Rafael Delgado Peña, Guillermo Boquizo y Javier Ponferrada
 * @version 1.0
 */
 
